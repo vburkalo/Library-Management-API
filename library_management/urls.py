c490d744758b0ca0b89bf39d29e64e5d28bc0ad0
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-07 15:14:14 +0300

    implemented users-crud

--- library_management/urls.py
+++ library_management/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('books.urls'))
+    path('api/', include('books.urls')),
+    path('api/users/', include('users.urls')),
 ]
