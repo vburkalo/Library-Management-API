cd4f6c236d806693fcf09ea48df5e50103c287b2
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-07 15:16:12 +0300

    changed api/books endpoint in project urls.py

--- library_management/urls.py
+++ library_management/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('books.urls')),
+    path('api/books/', include('books.urls')),
     path('api/users/', include('users.urls')),
 ]
