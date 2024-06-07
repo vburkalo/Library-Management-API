7db7e6bda5238e829eed232186100bd90e996bb8
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-07 14:05:50 +0300

    added books, users, borrowings, payments apps to INSTALLED_APPS in settings.py

--- library_management/settings.py
+++ library_management/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
+    'users',
+    'borrowings',
+    'payments',
 ]
 
 MIDDLEWARE = [
