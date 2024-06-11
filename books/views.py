2401182392f1eb687b37f859020e573cf0840952
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-11 14:35:29 +0300

    added permission classes to books views

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 from books.models import Book, Author
 from books.serializers import BookSerializer, AuthorSerializer
 
@@ -6,6 +8,7 @@ from books.serializers import BookSerializer, AuthorSerializer
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class BookViewSet(viewsets.ModelViewSet):
