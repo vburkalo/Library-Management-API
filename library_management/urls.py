839ae36cb793f43043c90593c578ed95b18768cb
vburkalo <vitaliyburkalo81@gmail.com>
2024-06-12 14:40:48 +0300

    added swagger & debug endpoints to project urls.py

--- library_management/urls.py
+++ library_management/urls.py
@@ -18,10 +18,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+    SpectacularAPIView,
+)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("books.urls")),
     path("api/users/", include("users.urls")),
     path("api/borrowings/", include("borrowings.urls")),
     path("api/payments/", include("payments.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
